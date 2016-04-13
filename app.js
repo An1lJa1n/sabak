1a2d9757783e5cac69ea50bf51d46c29b2ce119d
Anil Jain <AJain@cis.cisintranet.com>
2016-04-13 11:11:02 +0530

    text sms changes

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ var sess;
   app.use(bodyParser());  
   app.use(function(req, res, next) {
     res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
-    res.header("Access-Control-Allow-Origin", "http://sabak.herokuapp.com");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     if(req.url.indexOf(".jpg") > 0)
       next()
